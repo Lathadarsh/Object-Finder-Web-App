f7dc419853ba0dba62b44055574851f48511f623
Lathadarsh <97331941+Lathadarsh@users.noreply.github.com>
2023-04-15 17:50:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+status = "";
+objects = [];
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(380, 360);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380, 360);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 380, 360);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            percentage = Math.floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting Objects";
+    object_name = document.getElementById('name').value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
